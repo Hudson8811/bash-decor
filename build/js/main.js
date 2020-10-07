4444ddec66314e4503bc01b949e6e6ae39f0dc97
ArtZ <snickgem@gmail.com>
2020-10-07 20:05:31 +0400

    achievements block

--- build/js/main.js
+++ build/js/main.js
@@ -19,5 +19,30 @@ tpl:'<div class="fancybox-share"><h1>{{SHARE}}</h1><p><a class="fancybox-share__
 /* my scripts */
 
 $(document).ready(function () {
+	$(window).scroll(function () {
+		var pos = $('.achievements__counter').offset().top = window.innerHeight;
 
+		if ($(window).scrollTop() > pos) {
+			$('.achievements__digit').each(function () {
+				var that = $(this),
+						countTo = that.attr('data-count');
+
+				$({countNum: that.text()}).animate(
+					{
+						countNum: countTo
+					},
+					{
+						duration: 1000,
+						easing: 'swing',
+						step: function() {
+							that.text(Math.floor(this.countNum));
+						},
+						complete: function() {
+							that.text(this.countNum);
+						}
+					}
+				);
+			});
+		}
+	});
 });
--- src/js/partials/main.js
+++ src/js/partials/main.js
@@ -1,3 +1,28 @@
 $(document).ready(function () {
+	$(window).scroll(function () {
+		var pos = $('.achievements__counter').offset().top = window.innerHeight;
 
+		if ($(window).scrollTop() > pos) {
+			$('.achievements__digit').each(function () {
+				var that = $(this),
+						countTo = that.attr('data-count');
+
+				$({countNum: that.text()}).animate(
+					{
+						countNum: countTo
+					},
+					{
+						duration: 1000,
+						easing: 'swing',
+						step: function() {
+							that.text(Math.floor(this.countNum));
+						},
+						complete: function() {
+							that.text(this.countNum);
+						}
+					}
+				);
+			});
+		}
+	});
 });
